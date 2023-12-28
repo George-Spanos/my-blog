69eb3d7a6355671d0649f2cc902fe925038478a6
George Spanos <gspanos@moby-it.com>
2023-12-28 21:17:33 +0200

    update deps

--- src/site.config.ts
+++ src/site.config.ts
@@ -2,11 +2,11 @@ import type { SiteConfig } from "@/types";
 
 export const siteConfig: SiteConfig = {
 	// Used as both a meta property (src/components/BaseHead.astro L:31 + L:49) & the generated satori png (src/pages/og-image/[slug].png.ts)
-	author: "Chris Williams",
+	author: "George Spanos",
 	// Meta property used to construct the meta title property, found in src/components/BaseHead.astro L:11
-	title: "Astro Theme Cactus",
+	title: "George Spanos",
 	// Meta property used as the default description meta property
-	description: "An opinionated starter theme for Astro",
+	description: "Personal blog of George Spanos, founder of Moby IT",
 	// HTML lang property, found in src/layouts/Base.astro L:18
 	lang: "en-GB",
 	// Meta property, found in src/components/BaseHead.astro L:42
@@ -21,7 +21,7 @@ export const siteConfig: SiteConfig = {
 		},
 	},
 	webmentions: {
-		link: "https://webmention.io/astro-cactus.chriswilliams.dev/webmention",
+		link: "https://webmention.io/gspanos.tech/webmention",
 	},
 };
 
